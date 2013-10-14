f4feaa47b4c33bf4cfb6148a8aba80eae9cff4b7
Fernando Álvarez <alvarez.calleja.fernando@gmail.com>
2013-10-14 16:50:10 +0200

    First commit

---
+++ chat.py
@@ -0,0 +1,90 @@
+import os
+
+from gevent import monkey; monkey.patch_all()
+
+from socketio import socketio_manage
+from socketio.server import SocketIOServer
+from socketio.namespace import BaseNamespace
+from socketio.mixins import RoomsMixin, BroadcastMixin
+
+
+class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+    def on_nickname(self, nickname):
+        self.request['nicknames'].append(nickname)
+        self.socket.session['nickname'] = nickname
+        self.broadcast_event('announcement', '%s has connected' % nickname)
+        self.broadcast_event('nicknames', self.request['nicknames'])
+        # Just have them join a default-named room
+        self.join('main_room')
+
+    def recv_disconnect(self):
+        # Remove nickname from the list.
+        nickname = self.socket.session['nickname']
+        self.request['nicknames'].remove(nickname)
+        self.broadcast_event('announcement', '%s has disconnected' % nickname)
+        self.broadcast_event('nicknames', self.request['nicknames'])
+
+        self.disconnect(silent=True)
+
+    def on_user_message(self, msg):
+        self.emit_to_room('main_room', 'msg_to_room',
+            self.socket.session['nickname'], msg)
+
+    def recv_message(self, message):
+        print "PING!!!", message
+
+class Application(object):
+    def __init__(self):
+        self.buffer = []
+        # Dummy request object to maintain state between Namespace
+        # initialization.
+        self.request = {
+            'nicknames': [],
+        }
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO'].strip('/')
+
+        if not path:
+            try:
+                data = open('chat.html').read()
+            except Exception:
+                return not_found(start_response)
+
+            start_response('200 OK', [('Content-Type', 'text/html')])
+            return [data]
+
+
+        if path.startswith('static/'):
+            try:
+                data = open(path).read()
+            except Exception:
+                return not_found(start_response)
+
+            if path.endswith(".js"):
+                content_type = "text/javascript"
+            elif path.endswith(".css"):
+                content_type = "text/css"
+            else:
+                content_type = "text/html"
+
+            start_response('200 OK', [('Content-Type', content_type)])
+            return [data]
+
+        if path.startswith("socket.io"):
+            socketio_manage(environ, {'': ChatNamespace}, self.request)
+        else:
+            return not_found(start_response)
+
+
+def not_found(start_response):
+    start_response('404 Not Found', [])
+    return ['<h1>Not Found</h1>']
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8000))
+    print 'Listening on port {} and on port 843 (flash policy server)'.format(port)
+    SocketIOServer(('0.0.0.0', port), Application(),
+        resource="socket.io", policy_server=True,
+        policy_listener=('0.0.0.0', 10843)).serve_forever()
